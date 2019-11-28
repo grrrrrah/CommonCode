8dff7a08de8591bcffd92c6214640d3d2dd675be
lijiangop <lijiangop@163.com>
2019-11-28 14:47:44 +0800

    test1

---
+++ app/src/main/java/com/lijangop/commoncode/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lijangop.commoncode;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        
+    }
+}
